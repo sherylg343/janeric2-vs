08438a2f733a9721e7d3a77f620f68464dc063ca
sherylg343 <sheryl.s.goldberg@gmail.com>
2021-01-31 23:01:48 +0000

    uncommented favicon url code

--- janeric2/urls.py
+++ janeric2/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-#from django.contrib.staticfiles.storage import staticfiles_storage
-#from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 
 urlpatterns = [
@@ -32,7 +32,7 @@ urlpatterns = [
     path('shipping/', include('shipping.urls')),
     path('terms/', include('terms.urls')),
     path('about/', include('about.urls')),
-    #path('favicon.ico', RedirectView.as_view(
-    #   url=staticfiles_storage.url('img/favicon.ico'))),
+    path('favicon.ico', RedirectView.as_view(
+       url=staticfiles_storage.url('img/favicon.ico'))),
     ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
