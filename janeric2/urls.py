d183bad250583a157617f4261baf70d9d82da1e8
sherylg343 <sheryl.s.goldberg@gmail.com>
2021-01-31 02:48:02 +0000

    renamed janeric folder janeric2 and changed settings

--- janeric2/urls.py
+++ janeric2/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.staticfiles.storage import staticfiles_storage
-from django.views.generic.base import RedirectView
+#from django.contrib.staticfiles.storage import staticfiles_storage
+#from django.views.generic.base import RedirectView
 
 
 urlpatterns = [
