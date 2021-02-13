6e80cb0e9a4112ac03c721c2a164a1ad390faf4b
sherylg343 <sheryl.s.goldberg@gmail.com>
2021-02-13 00:30:19 +0000

    changes to email backend in settings

--- janeric2/settings.py
+++ janeric2/settings.py
@@ -218,17 +218,17 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY2', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY2', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET2', '')
 
-if 'DEVELOPMENT2' in os.environ:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'janericllc@example.com'
-else:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLS = True
-    EMAIL_PORT = 587
-    EMAIL_HOST = 'smtp.sendgrid.net'
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_FROM')
+#if 'DEVELOPMENT2' in os.environ:
+#    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#    DEFAULT_FROM_EMAIL = 'janericllc@example.com'
+#else:
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY')
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_FROM')
 
 # Allauth email settings
 #ACCOUNT_EMAIL_REQUIRED = True
