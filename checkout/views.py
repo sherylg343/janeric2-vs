744c7ae01f98c0b12a0d6fa9280a53eddc646dc9
sherylg343 <sheryl.s.goldberg@gmail.com>
2021-05-20 12:20:57 +0000

    fixed typo line 154 checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -151,7 +151,7 @@ def checkout(request):
     ship_state = USStateSelect()
     bill_state = USStateSelect()
     ship_zipcode = USZipCodeField()
-    bill_zipcode = USZipCodeField(s
+    bill_zipcode = USZipCodeField()
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
