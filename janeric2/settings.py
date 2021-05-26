d36c186a82fd151451b992e6f27c5d50375f46da
sherylg343 <sheryl.s.goldberg@gmail.com>
2021-05-26 08:56:15 +0000

    fixed indentation error for email settings

--- janeric2/settings.py
+++ janeric2/settings.py
@@ -223,13 +223,13 @@ if 'DEVELOPMENT2' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'janericllc@example.com'
 else:
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY')
-DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_FROM')
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.sendgrid.net'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_FROM')
 
 # Allauth email settings
 ACCOUNT_EMAIL_REQUIRED = True
