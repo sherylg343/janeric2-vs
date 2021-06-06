a69fbb411d5291334819a079bb148c026d80d354
sherylg343 <sheryl.s.goldberg@gmail.com>
2021-06-06 14:09:25 +0000

    js sidemenu scrolls before footer covers it

--- static/js/base.js
+++ static/js/base.js
@@ -17,3 +17,37 @@ $(document).ready(function() {
         });
     });
 });
+$(window).on('scroll', function() {
+    releaseFixedSidebar();
+});
+
+/* Based upon "Make a Sidebar Always Visible Regardless of Scroll",
+by Łukasz Nojek, 4-20-20, https://lukasznojek.com/blog/2020/04/make-a-sidebar-always-visible-regardless-of-scroll/ */
+function releaseFixedSidebar() {
+    const footer = $("footer");
+    const topOfFooter = footer.offset().top;
+    console.log("----top of footer:", topOfFooter);
+    const sideMenuColumn = $(".side-menu-col");
+    const sideMenu = $(".side-menu")
+    const sideMenuTop = sideMenu.position().top;
+    console.log("----sideMenu top:", sideMenuTop);
+    const sideMenuHeight = sideMenu.outerHeight(false);
+    const sideMenuBottom = sideMenuTop + sideMenuHeight;
+    console.log("----sideMenu bottom:", sideMenuBottom);
+    headerHeight = $("header").outerHeight(false);
+    scrollPosition = window.scrollY;
+    sideMenuCalc = scrollPosition + (headerHeight + sideMenuHeight);
+    console.log("---sideMenuCalc:", sideMenuCalc);
+    console.log("scrollTop:", scrollPosition);
+    const myWindow = $(window).height();
+    console.log("window height:", window);
+    if(sideMenuCalc < topOfFooter) {
+        $(sideMenuColumn).css({"position": "fixed"});
+        $(".head-row").css({"transform": "translateX(30vw)"});
+        console.log("side fixed");
+    } else if((myWindow < sideMenuHeight) || (sideMenuCalc >= topOfFooter)) {
+        $(sideMenuColumn).css({"position": "relative"});
+        $(".head-row").css({"transform": "translateX(7vw)"});
+        console.log("side relative - scrolls");
+    }
+}
