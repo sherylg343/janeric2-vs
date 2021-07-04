86fe563a1b077e50aa900280b9809035d83fcaf9
sherylg343 <sheryl.s.goldberg@gmail.com>
2021-07-04 17:04:20 +0000

    settings debug set to false

--- janeric2/settings.py
+++ janeric2/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY2', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT2' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = ['janeric2.herokuapp.com', 'localhost']
 
