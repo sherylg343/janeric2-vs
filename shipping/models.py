2beb3f82ca0d7228b004f5cba5ab5f337be54170
sherylg343 <sheryl.s.goldberg@gmail.com>
2021-09-11 19:51:50 +0000

    specified plural admin name for terms and shipping

--- shipping/models.py
+++ shipping/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Shipping(models.Model):
+    class Meta:
+        verbose_name_plural = 'Shipping'
 
     title = models.CharField(max_length=80)
     subtitle = models.CharField(max_length=80, blank=True, null=True)
--- terms/models.py
+++ terms/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 
 class Terms(models.Model):
+    class Meta:
+        verbose_name_plural = 'Terms'
 
     title = models.CharField(max_length=80)
     subtitle = models.CharField(max_length=80, blank=True, null=True)
