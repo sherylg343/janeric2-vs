d6c790f70f491b832b43a403331b02858bc89dce
sherylg343 <sheryl.s.goldberg@gmail.com>
2021-09-11 19:43:57 +0000

    added terms model

--- terms/admin.py
+++ terms/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Terms
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+class TermsAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'subtitle', 'body', 'created_on', 'modified_on')
+    summernote_fields = ('body',)
+
+admin.site.register(Terms, TermsAdmin)
--- terms/models.py
+++ terms/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Terms(models.Model):
+
+    title = models.CharField(max_length=80)
+    subtitle = models.CharField(max_length=80, blank=True, null=True)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
