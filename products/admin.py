075349e61e0d1dd51280b1be1dad3989dc8bf996
sherylg343 <sheryl.s.goldberg@gmail.com>
2021-09-11 16:50:21 +0000

    imported ProductSize model to admin file

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category, Product_Family
+from .models import Product, Category, Product_Family, ProductSize
 
 
 # Register your models here.
